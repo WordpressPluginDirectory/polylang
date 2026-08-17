2e29fdd2605f4287b84efeee0af6c7e4b3006e8b
Justin Gardner <justin@rhynorater.com>
2026-08-17 06:01:28 -0500

    3.8.7

--- polylang/js/build/blocks.js
+++ polylang/js/build/blocks.js
@@ -121,17 +121,17 @@ module.exports = (function() { return this["wp"]["primitives"]; }());
 /******/ 	});
 /************************************************************************/
 /******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
+/******/ 	const __webpack_module_cache__ = {};
 /******/ 	
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
 /******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		const cachedModule = __webpack_module_cache__[moduleId];
 /******/ 		if (cachedModule !== undefined) {
 /******/ 			return cachedModule.exports;
 /******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 		const module = __webpack_module_cache__[moduleId] = {
 /******/ 			// no module.id needed
 /******/ 			// no module.loaded needed
 /******/ 			exports: {}
@@ -149,7 +149,7 @@ module.exports = (function() { return this["wp"]["primitives"]; }());
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
 /******/ 		__webpack_require__.n = (module) => {
-/******/ 			var getter = module && module.__esModule ?
+/******/ 			const getter = module && module.__esModule ?
 /******/ 				() => (module['default']) :
 /******/ 				() => (module);
 /******/ 			__webpack_require__.d(getter, { a: getter });
@@ -159,11 +159,26 @@ module.exports = (function() { return this["wp"]["primitives"]; }());
 /******/ 	
 /******/ 	/* webpack/runtime/define property getters */
 /******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
+/******/ 		// define getter/value functions for harmony exports
 /******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 			if(Array.isArray(definition)) {
+/******/ 				var i = 0;
+/******/ 				while(i < definition.length) {
+/******/ 					var key = definition[i++];
+/******/ 					var binding = definition[i++];
+/******/ 					if(!__webpack_require__.o(exports, key)) {
+/******/ 						if(binding === 0) {
+/******/ 							Object.defineProperty(exports, key, { enumerable: true, value: definition[i++] });
+/******/ 						} else {
+/******/ 							Object.defineProperty(exports, key, { enumerable: true, get: binding });
+/******/ 						}
+/******/ 					} else if(binding === 0) { i++; }
+/******/ 				}
+/******/ 			} else {
+/******/ 				for(var key in definition) {
+/******/ 					if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 						Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 					}
 /******/ 				}
 /******/ 			}
 /******/ 		};
@@ -175,7 +190,7 @@ module.exports = (function() { return this["wp"]["primitives"]; }());
 /******/ 	})();
 /******/ 	
 /************************************************************************/
-var __webpack_exports__ = {};
+let __webpack_exports__ = {};
 // This entry needs to be wrapped in an IIFE because it needs to be in strict mode.
 (() => {
 "use strict";
@@ -337,6 +352,60 @@ function createLanguageSwitcherEdit(props) {
 }
 // EXTERNAL MODULE: external {"this":["wp","element"]}
 var external_this_wp_element_ = __webpack_require__(601);
+// EXTERNAL MODULE: external {"this":["wp","data"]}
+var external_this_wp_data_ = __webpack_require__(987);
+;// ./js/src/blocks/hooks/use-current-language-with-editor-context.js
+/**
+ * WordPress dependencies
+ */
+
+
+/**
+ * Finds a language by slug.
+ *
+ * @param {Array}  languages The languages list.
+ * @param {string} slug      The language slug.
+ * @return {Object|null} The language, `null` if not found.
+ */
+const findLanguageBySlug = (languages, slug) => {
+  if (!languages || !slug) {
+    return null;
+  }
+  const currentLanguage = languages.find(language => {
+    return language.slug === slug;
+  });
+  return currentLanguage ? currentLanguage : null;
+};
+
+/**
+ * Resolves the current language in block editor contexts.
+ *
+ * The widgets editor does not load `@wordpress/editor`, so the `core/editor`
+ * store may be unavailable. In that context, fall back to the current language
+ * slug injected by PHP (default language when none is set).
+ *
+ * @param {Array} languages The languages list.
+ * @return {Object|null} The current language, `null` if not found.
+ */
+const useCurrentLanguageWithEditorContext = languages => {
+  return (0,external_this_wp_data_.useSelect)(select => {
+    if (!languages) {
+      return null;
+    }
+    const editorStore = select('core/editor');
+    if (!editorStore) {
+      return findLanguageBySlug(languages, pllEditorCurrentLanguageSlug // eslint-disable-line no-undef
+      );
+    }
+    const currentPost = editorStore.getCurrentPost();
+    if (!currentPost) {
+      return null;
+    }
+    const currentLanguageSlug = currentPost.lang ?? pllEditorCurrentLanguageSlug; // eslint-disable-line no-undef
+
+    return findLanguageBySlug(languages, currentLanguageSlug);
+  }, [languages]);
+};
 ;// ./js/src/blocks/languages-context.js
 /**
  * WordPress dependencies.
@@ -465,37 +534,6 @@ const SwitcherUI = ({
     })
   });
 };
-// EXTERNAL MODULE: external {"this":["wp","data"]}
-var external_this_wp_data_ = __webpack_require__(987);
-// EXTERNAL MODULE: external {"this":["wp","editor"]}
-var external_this_wp_editor_ = __webpack_require__(2);
-;// ./node_modules/@wpsyntex/polylang-react-library/build/hooks/use-current-language.js
-/**
- * WordPress dependencies
- */
-
-// This package is not found in `@wordpress/scripts` like others (peer dependency).
-// eslint-disable-next-line import/no-unresolved
-
-
-/**
- * Custom hook to get the current language from the editor.
- *
- * @param {Array} languages The languages list.
- * @return {Object|null} The current language, `null` if not found.
- */
-const useCurrentLanguage = languages => {
-  const currentPost = (0,external_this_wp_data_.useSelect)(select => select(external_this_wp_editor_.store).getCurrentPost());
-  if (!languages || !currentPost) {
-    return null;
-  }
-  const currentLanguageSlug = currentPost.lang ?? pllEditorCurrentLanguageSlug; // eslint-disable-line no-undef
-
-  const currentLanguage = languages.find(language => {
-    return language.slug === currentLanguageSlug;
-  });
-  return currentLanguage ? currentLanguage : null;
-};
 ;// ./node_modules/@wpsyntex/polylang-react-library/build/hooks/use-curated-languages.js
 /**
  * WordPress dependencies
@@ -538,6 +576,7 @@ const useCuratedLanguages = (languages, currentLanguage, reduceToOneItem) => {
 
 
 
+
 /**
  * Switcher container component.
  *
@@ -557,7 +596,7 @@ const SwitcherContainer = ({
   const {
     languages
   } = (0,external_this_wp_element_.useContext)(LanguagesContext);
-  const currentLanguage = useCurrentLanguage(languages);
+  const currentLanguage = useCurrentLanguageWithEditorContext(languages);
   const curatedLanguages = useCuratedLanguages(languages, currentLanguage, dropdown);
   return /*#__PURE__*/(0,jsx_runtime.jsx)(SwitcherUI, {
     languages: curatedLanguages,
@@ -782,6 +821,35 @@ const switcher_ui_SwitcherUI = ({
       }, language.slug);
     })
   });
+};
+// EXTERNAL MODULE: external {"this":["wp","editor"]}
+var external_this_wp_editor_ = __webpack_require__(2);
+;// ./node_modules/@wpsyntex/polylang-react-library/build/hooks/use-current-language.js
+/**
+ * WordPress dependencies
+ */
+
+// This package is not found in `@wordpress/scripts` like others (peer dependency).
+// eslint-disable-next-line import/no-unresolved
+
+
+/**
+ * Custom hook to get the current language from the editor.
+ *
+ * @param {Array} languages The languages list.
+ * @return {Object|null} The current language, `null` if not found.
+ */
+const useCurrentLanguage = languages => {
+  const currentPost = (0,external_this_wp_data_.useSelect)(select => select(external_this_wp_editor_.store).getCurrentPost());
+  if (!languages || !currentPost) {
+    return null;
+  }
+  const currentLanguageSlug = currentPost.lang ?? pllEditorCurrentLanguageSlug; // eslint-disable-line no-undef
+
+  const currentLanguage = languages.find(language => {
+    return language.slug === currentLanguageSlug;
+  });
+  return currentLanguage ? currentLanguage : null;
 };
 ;// ./js/src/blocks/navigation-language-switcher/components/navigation-switcher-container.js
 /**
